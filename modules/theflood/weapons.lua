a7a7db6ffe0582557f2b71165cebc95da79bdbdc
markmcfuzz <magecape10@gmail.com>
2021-02-22 02:11:48 -0600

    assault rifle crosshair animations added

--- modules/theflood/weapons.lua
+++ modules/theflood/weapons.lua
@@ -2,41 +2,102 @@ local core = require "theflood.core"
 
 local weapons = {}
 
-local animatedCrosshairsTags = {
-    core.findTag("fuel_rod", tagClasses.weaponHudInterface).path,
-    core.findTag("shotgun_m90", tagClasses.weaponHudInterface).path
+local animatedCrosshairsTagsCommon = {
+    core.findTag("smg_m7a", tagClasses.weaponHudInterface).path,
 }
 
+local animatedCrosshairsTagsLarger = {
+    core.findTag("assault_rifle_ma38", tagClasses.weaponHudInterface).path,
+}
+
+
+    --Crosshair SMG
 function weapons.animateCrosshairs()
-    for crosshairNumber, crosshairTagPath in pairs(animatedCrosshairsTags) do
+    for crosshairNumber, crosshairTagPath in pairs(animatedCrosshairsTagsCommon) do
         local weaponHud = blam.weaponHudInterface(crosshairTagPath)
         local crosshairs = weaponHud.crosshairs
         local player = blam.biped(get_dynamic_player())
         if (player and weaponHud) then
             local newCrosshairs = weaponHud.crosshairs
             if (player.shooting == 1) then
-                local currentScale = {
-                    width = newCrosshairs[1].overlays[1].widthScale,
-                    height = newCrosshairs[1].overlays[1].heightScale
-                }
-                newCrosshairs[1].overlays[1].widthScale = currentScale.width * 1.2
-                newCrosshairs[1].overlays[1].heightScale = currentScale.height * 1.2
+                --local currentScale = {
+                    --width = newCrosshairs[1].overlays[1].widthScale,
+                    --height = newCrosshairs[1].overlays[1].heightScale
+                --}
+                --console_out("Dynamic Crosshair Working")
+                newCrosshairs[1].overlays[1].x = -7
+                newCrosshairs[2].overlays[1].x = 7
+                newCrosshairs[3].overlays[1].y = -7
+                newCrosshairs[4].overlays[1].y = 7
+                newCrosshairs[5].overlays[1].widthScale = 0.235
+                newCrosshairs[5].overlays[1].heightScale = 0.235
+                newCrosshairs[6].overlays[1].widthScale = 0.135
+                newCrosshairs[6].overlays[1].heightScale = 0.135
+                weaponHud.crosshairs = newCrosshairs
             else
-                local currentScale = {
-                    width = newCrosshairs[1].overlays[1].widthScale,
-                    height = newCrosshairs[1].overlays[1].heightScale
-                }
-                newCrosshairs[1].overlays[1].widthScale = 0.25
-                newCrosshairs[1].overlays[1].heightScale = 0.25
+                --local currentScale = {
+                    --width = newCrosshairs[1].overlays[1].widthScale,
+                    --height = newCrosshairs[1].overlays[1].heightScale
+                --}
+                newCrosshairs[1].overlays[1].x = 0
+                newCrosshairs[2].overlays[1].x = 0
+                newCrosshairs[3].overlays[1].y = 0
+                newCrosshairs[4].overlays[1].y = 0
+                newCrosshairs[5].overlays[1].widthScale = 0.25
+                newCrosshairs[5].overlays[1].heightScale = 0.25
+                newCrosshairs[6].overlays[1].widthScale = 0.001
+                newCrosshairs[6].overlays[1].heightScale = 0.001
             end
             weaponHud.crosshairs = newCrosshairs
-        end
+        end     
+    end
+end
+
+    --Crosshair Assaul Rifle
+function weapons.animateCrosshairsAr()
+    for crosshairNumber, crosshairTagPath in pairs(animatedCrosshairsTagsLarger) do
+        local weaponHud = blam.weaponHudInterface(crosshairTagPath)
+        local crosshairs = weaponHud.crosshairs
+        local player = blam.biped(get_dynamic_player())
+        if (player and weaponHud) then
+            local newCrosshairs = weaponHud.crosshairs
+            if (player.shooting == 1) then
+                --local currentDisplace = {
+                    --xMove = newCrosshairs[1].overlays[1].x
+                    --yMove = newCrosshairs[1].overlays[1].y
+                --}
+                --console_out("Dynamic Crosshair Working")
+                newCrosshairs[1].overlays[1].x = -10
+                newCrosshairs[2].overlays[1].x = 10
+                newCrosshairs[3].overlays[1].y = -10
+                newCrosshairs[4].overlays[1].y = 10
+                newCrosshairs[5].overlays[1].widthScale = 0.235
+                newCrosshairs[5].overlays[1].heightScale = 0.235
+                newCrosshairs[6].overlays[1].widthScale = 0.135
+                newCrosshairs[6].overlays[1].heightScale = 0.135
+                weaponHud.crosshairs = newCrosshairs
+            else
+                --local currentScale = {
+                    --width = newCrosshairs[1].overlays[1].widthScale,
+                    --height = newCrosshairs[1].overlays[1].heightScale
+                --}
+                newCrosshairs[1].overlays[1].x = 0
+                newCrosshairs[2].overlays[1].x = 0
+                newCrosshairs[3].overlays[1].y = 0
+                newCrosshairs[4].overlays[1].y = 0
+                newCrosshairs[5].overlays[1].widthScale = 0.25
+                newCrosshairs[5].overlays[1].heightScale = 0.25
+                newCrosshairs[6].overlays[1].widthScale = 0.001
+                newCrosshairs[6].overlays[1].heightScale = 0.001
+            end
+            weaponHud.crosshairs = newCrosshairs
+        end     
     end
 end
 
 local rotation = 0
 
-function weapons.rotateWeaponsInPads()
+function weapons.rotateWeaponsInPads() --Weapon Pad for Rotating weapons
     -- Local scope variables
     if (rotation < 360) then
         rotation = rotation + 1
