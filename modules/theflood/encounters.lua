2ab29f00e6eb5daa964e736158339e4b2240c082
Sled <angelherxd@hotmail.com>
2021-03-08 11:58:46 -0700

    fixed encounter names

--- modules/theflood/encounters.lua
+++ modules/theflood/encounters.lua
@@ -15,15 +15,19 @@ Will result into:
 console_out("Oleada1_OpA")
 
 ]]
-local encounterNameTemplate = "encounter_block_%s"
---local encounterNameTemplate = "Oleada%s_Op%s"
+--local encounterNameTemplate = "encounter_block_%s"
+local encounterNameTemplate = "Oleada%s_Op%s"
+local uglyHack = {"A", "B", "C"}
+
 
 function encounters.gimmecovies()
     local player = blam.biped(get_dynamic_player())
     if (player) then
         if (player.actionKey) then
-            local randomEncounterIndex = math.random(1, 1)
-            hsc.aiPlace(encounterNameTemplate:format(randomEncounterIndex, "A"))
+            local randomEncounterIndex = math.random(1, 3)
+            local randomEncounterName = encounterNameTemplate:format(randomEncounterIndex, uglyHack[math.random(1,3)])
+            console_out(randomEncounterName)
+            hsc.aiPlace(randomEncounterName)
             --hsc.aiMagicallySeePlayers("drones")
         end
     end
